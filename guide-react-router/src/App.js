4929f2307fdc50f38f7b112e0ff6a7097c6fb1e2
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-05-28 17:48:04 +0300

    feat: add MainPage component

--- guide-react-router/src/App.js
+++ guide-react-router/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PageOne, PageTwo } from './Pages';
+import { MainPage } from './MainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<MainPage />} />
         <Route path="*" element={<PageOne />} />
         <Route path="two" element={<PageTwo />} />
       </Routes>
